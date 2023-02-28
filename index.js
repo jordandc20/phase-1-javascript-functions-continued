d6bf7a031fb3ba9e7c3885b0461ca8f403878ec0
Diana Jordan <dianacjordan@gmail.com>
2023-02-28 13:27:41 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+     let sat=`This Saturday, I want to ${activity}!`
+     return sat
+}
+
+
+
+const mondayWork = function (activity = 'go to the office') {
+let mon =   `This Monday, I will ${activity}.`
+return mon
+}
+
+function wrapAdjective (flair='*'){
+    return function inner(adj='special') {
+return `You are ${flair}${adj}${flair}!`
+
+
+
+    }
+}
+
+
+
